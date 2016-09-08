8ee8f74aa112b660be9059ea0f645ae9e20b2347
Arun Chandran <achandran@mvista.com>
2016-09-08 15:05:00 +0530

    modify auto_cherry.sh
    changes are
    a) Introduced options -b and -f
    b) removed temparary use of a file instead use sed inplace replacement

--- linux_general_setup/useful_shell_scripts/auto_cherry.sh
+++ linux_general_setup/useful_shell_scripts/auto_cherry.sh
@@ -1,64 +1,92 @@
 usage () {
-	echo -e "\nUsage: auto_cherry.sh <commit_series_file> <BUGID>"
+	echo "Usage: auto_cherry.sh [Options]"
+	echo "If you are cherry-picking a bunch of commits, use auto_cherry.sh to make it easy"
+	echo -e "\nEg: \$$0 -f <commit_series_file> -b <bug number>"
+	echo -e "\nOptions:"
+	echo -e "\t-f, The file containg the commits. It only takes one commits per line
+       	 If you have 10 commits to cherry-pick the series file should have 10 lines, with each git commit IDs forming a line.
+	 The commits should be arranged in the order in which they needs to be cherry-picked"
+	echo -e "\t-b, The bugID"
 	exit 1
 }
 
-if [ -n "$1" ]  && [ -n "$2" ] ; then
-	if [ ! -f "$1" ] ; then
-		echo "$1 is not a commit series file"
-		echo "Please supply a commit series file as arg1 and BUGID as arg2"
+while getopts  "f:b:h" OPTION
+do
+	case $OPTION in
+	h)
 		usage
-	fi
+		exit 1
+		;;
+	f)
+		commit_series_file=$OPTARG
+		if [ ! -f "$commit_series_file" ] ; then
+			echo "$commit_series_file is not a commit series file"
+			echo "-f needs a commit series file as the argument"
+			usage
+		else
+			echo "Using $commit_series_file as the commit series"
+			COMMIT_LOG="$commit_series_file""_auto_cherry_commit_log"
+		fi
 
-	echo "Using $1 as the commit series"
-	COMMIT_LOG=$1_auto_cherry_commit_log
-	COMMIT_LOG_SAVE=$COMMIT_LOG"_save"
-	if [ -f $COMMIT_LOG ] ; then
-		echo "Using the already existing $COMMIT_LOG file"
-	else
-		echo "Creating new commit fiew $COMMIT_LOG"
-		cat $1  | tac | sed -e 's/$/& [apply]/g' > $COMMIT_LOG
-	fi
-else
+		if [ -f $COMMIT_LOG ] ; then
+			echo "Using the already existing $COMMIT_LOG file"
+		else
+			echo "Creating new commit file $COMMIT_LOG"
+			cat $commit_series_file | sed -e 's/$/& [apply]/g' > $COMMIT_LOG
+		fi
+
+		;;
+	b)
+		BUG="$OPTARG"
+		;;
+	?)
+		usage
+		exit
+		;;
+	esac
+done
+
+if [ -z $COMMIT_LOG ] ; then
+	echo "please pass the commit series file using -f option"
 	usage
 fi
 
-BUG="$2"
+if [ -z "$BUG" ] ; then
+	echo "please pass the bug number using -b option"
+	usage
+fi
+
 SOURCE="kernel.org"
 MSG="Backport from"
 TYPE="Integration"
 
 total_lines=$(wc -l $COMMIT_LOG | cut -d' ' -f1)
-no_of_lines=0
+line_no=0
 
-echo "Total no of lines to process = $total_lines"
+echo "Total no of commits to process: $total_lines"
 while read line <&3
 do
-	no_of_lines=$((no_of_lines + 1 ))
+	line_no=$((line_no + 1 ))
 	APPLY=$(echo $line | cut -d' ' -f2 | sed -e 's/\[//' -e 's/\]//')
 	if [ $APPLY == "apply" ] ; then
 		COMMIT_ID=$(echo $line | cut -d' ' -f1)
 		DISC=$(git describe --contains $COMMIT_ID | cut -d~ -f1)
 
-		echo "using $COMMIT_ID"
-
+		echo "Cherry-picking $COMMIT_ID"
 		git-cherry-pick-mv --bugz $BUG --source "$SOURCE" --disposition "$MSG $DISC" --type "$TYPE" $COMMIT_ID
 
 		if [ $? -ne 0 ] ; then
 			echo -e "\nAttention cherry-pick failed for $COMMIT_ID"
-			echo $line | sed -e 's/\[apply\]/\[failed\]/' >> $COMMIT_LOG_SAVE
+			sed -i "$line_no""s/\[apply\]/\[failed\]/" $COMMIT_LOG
 			break
 		else
-			echo $line | sed -e 's/\[apply\]/\[applied\]/' >> $COMMIT_LOG_SAVE
+			sed -i "$line_no""s/\[apply\]/\[applied\]/" $COMMIT_LOG
 		fi
 	else
-		echo "$line" >> $COMMIT_LOG_SAVE
+		COMMIT_ID=$(echo $line | cut -d' ' -f1)
+		echo "No action to do for $COMMIT_ID; It said $line"
 	fi
 done 3<"$COMMIT_LOG"
 
-echo "processed = $no_of_lines"
-no_of_lines=$((no_of_lines + 1 ))
-
-sed -n $no_of_lines,$total_lines"p" $COMMIT_LOG >> $COMMIT_LOG_SAVE
-mv $COMMIT_LOG_SAVE $COMMIT_LOG
+echo "processed = $line_no"
 exit 0
