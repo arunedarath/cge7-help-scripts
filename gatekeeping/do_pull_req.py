c22ea07ce574e018fcae1f73256aac3c47e55134
Arun Chandran <achandran@mvista.com>
2016-10-02 00:45:36 +0530

    Add dynamic repo identification
    This will choose where to push depending on the repository

--- gatekeeping/do_pull_req.py
+++ gatekeeping/do_pull_req.py
@@ -13,22 +13,38 @@ parser.add_argument('-r', '--revision', help='Patch revision', required=True, ty
 parser.add_argument('-n', '--num', help='No of patches you are pushing', required=True, type=int)
 args = vars(parser.parse_args())
 
-kgit = "gitcge7.mvista.com:/mvista/git/cge7/contrib/kernel.git"
-msg = "Merge to mvl7-3.10/cge_dev"
-url = "git://gitcge7.mvista.com/cge7/contrib/kernel.git"
-
 bug_no = str(args['bugz'])
 revision = str(args['revision'])
 p_count = str(args['num'])
 
+repo_details = [
+        {
+            'repo': 'CGE7',
+            'origin': 'gitcge7.mvista.com:/mvista/git/cge7/kernel/mvl7kernel.git',
+            'pushto': 'gitcge7.mvista.com:/mvista/git/cge7/contrib/kernel.git',
+            'tag_msg': 'Merge to mvl7-3.10/cge_dev',
+            'git_tag': 'mvl7-3.10/cge_dev-',
+            'url': 'git://gitcge7.mvista.com/cge7/contrib/kernel.git',
+            },
+        {
+            'repo': 'CGX2.0',
+            'origin': 'gitcgx.mvista.com:/mvista/git/cgx/CGX2.0/kernel/linux-mvista-2.0.git',
+            },
+        ]
+
 bugz_login_url = 'http://bugz.mvista.com/show_bug.cgi?id='+bug_no
 bugz_post_url = 'http://bugz.mvista.com/process_bug.cgi'
 
-tag = 'mvl7-3.10/cge_dev-'+bug_no+'-'+'V'+revision
 start_c = 'HEAD~'+p_count
 merg_req_f = bug_no+'-'+'V'+revision+'.file'
 
 cmd_timeout = 180
+
+
+def error_exit(prstr):
+        print(prstr)
+        print("Exiting..")
+        sys.exit(1)
 
 
 def execute_cmd(cmd, prstr):
@@ -39,22 +55,59 @@ def execute_cmd(cmd, prstr):
         if (p.returncode == 0):
             print (prstr)
         else:
-            print ("The command:%s pid:%d failed... exiting \n" % (cmd, p.pid))
-            sys.exit(1)
+            error_exit("The command:%s pid:%d failed" % (cmd, p.pid))
     except subprocess.TimeoutExpired:
         p.kill()
         print ("The command:%s pid:%d timed out, but continuing \n" % (cmd, p.pid))
         return
 
+
+def find(lst, key, value):
+    for i, dic in enumerate(lst):
+        if dic[key] == value:
+            return i
+    return -1
+
+
+def form_repo_data(idx):
+    print("%s repo identified" % (repo_details[idx]['repo']))
+    global contrib, msg, contrib_url, tag
+    contrib = repo_details[idx]['pushto']
+    msg = repo_details[idx]['tag_msg']
+    contrib_url = repo_details[idx]['url']
+    tag = repo_details[idx]['git_tag']+bug_no+'-V'+revision
+
+
+def identify_repo():
+    cmd = 'git config --get remote.origin.url > %s' % (merg_req_f)
+    rc = execute_cmd(cmd, "")
+    txt = open(merg_req_f)
+    tmp = txt.readline()
+    txt.close()
+    tmp = tmp.splitlines()
+    if (tmp):
+        tmp = tmp[0].split('@')
+        if (len(tmp) != 2):
+            error_exit("Not an MV type repo\n")
+
+        idx = find(repo_details, 'origin', tmp[1])
+        if (idx == -1):
+            error_exit("Unable to find the repo type\n")
+
+        form_repo_data(idx)
+    else:
+        error_exit("Not an MV type repo\n")
+
+identify_repo()
 cmd = 'git tag -m "%s" "%s"' % (msg, tag)
 execute_cmd(cmd, "Successfully created tag : %s\n" % (tag))
 
-print ("Pushing the tag : %s to %s" % (tag, url))
+print ("Pushing the tag : %s to %s" % (tag, contrib_url))
 username = input("Please enter your bugzilla user name\n")
-cmd = 'git push "%s"@"%s" "+%s"' % (username, kgit, tag)
-execute_cmd(cmd, "Successfully pushed the tag to %s\n" % (kgit))
+cmd = 'git push "%s"@"%s" "+%s"' % (username, contrib, tag)
+execute_cmd(cmd, "Successfully pushed the tag to %s\n" % (contrib))
 
-cmd = 'git request-pull %s %s %s | tee %s' % (start_c, url, tag, merg_req_f)
+cmd = 'git request-pull %s %s %s | tee %s' % (start_c, contrib_url, tag, merg_req_f)
 execute_cmd(cmd, "Successfully generated pull request\n")
 
 print ("Trying to update bugzilla fields for bug %s" % str(args['bugz']))
@@ -72,17 +125,9 @@ upd_d = {
 }
 
 
-def handle_bugz_err(prstr):
-        print(prstr)
-        print("Exiting..")
-        sys.exit(1)
-
-
 def check_err(err, got):
     if (err == got):
-        print (err)
-        print("Exiting..")
-        sys.exit(1)
+        error_exit(err)
 
 with requests.session() as s:
     try:
@@ -97,10 +142,10 @@ with requests.session() as s:
 
         txt = open(merg_req_f)
         upd_d['comment'] = txt.read()
+        txt.close()
         r = s.post(bugz_post_url, data=upd_d)
         print("Finished....... bye.")
     except requests.exceptions.Timeout:
-        handle_bugz_err("Connection timed out")
+        error_exit("Connection timed out")
     except requests.exceptions.RequestException as e:
-        handle_bugz_err(e)
-        sys.exit(1)
+        error_exit(e)
