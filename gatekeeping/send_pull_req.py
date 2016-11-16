cf5d783b625049cb6015907f1e40df18c0ee23ed
Arun Chandran <achandran@mvista.com>
2016-11-17 00:22:32 +0530

    Introduce run_cmd()
    This makes executing git commands simple and eliminates the use of a tmp
    file

--- gatekeeping/send_pull_req.py
+++ gatekeeping/send_pull_req.py
@@ -7,6 +7,7 @@ import subprocess
 import getpass
 import requests
 import pexpect
+import re
 
 bug_no = ''
 revision = ''
@@ -18,7 +19,6 @@ contrib = ''
 msg = ''
 contrib_url = ''
 tag = ''
-tmp_f = ''
 username = ''
 mvista_id = ''
 debug = ''
@@ -48,35 +48,34 @@ repo_details = [
 
 def dbg_print(str):
     if (debug == 1):
-        print(str)
+        if (str):
+            print(str)
 
 
 def error_exit(prstr):
     print(prstr)
     print("Exiting..")
     if (tag_is_created == 1):
-        cmd = 'git tag -d %s > /dev/null' % (tag)
-        execute_cmd(cmd, "", "")
+        run_cmd('git tag -d %s > /dev/null' % (tag))
     sys.exit(1)
 
 
-def execute_cmd(cmd, s_str, f_str):
-    cmd_timeout = 180
-    "Execute the bash commands encoded in cmd and handle timeouts"
+def run_cmd(cmd, *argv):
+    leng = len(argv)
+    sstr = ""
+    fstr = ""
+
+    if (leng):
+        fstr = argv[0]
+        if (leng > 1):
+            sstr = argv[1]
+
     try:
-        p = subprocess.Popen(cmd, shell=True)
-        p.wait(timeout=cmd_timeout)
-        if (p.returncode == 0):
-            if (s_str):
-                dbg_print(s_str)
-        else:
-            if (f_str):
-                print(f_str)
-            error_exit('Unable to run command: %s' % (cmd))
-    except subprocess.TimeoutExpired:
-        p.kill()
-        print("The command:%s pid:%d timed out, but continuing \n" % (cmd, p.pid))
-        return
+        op = subprocess.check_output(cmd, shell=True, universal_newlines=True)
+        dbg_print(sstr)
+        return op
+    except subprocess.CalledProcessError as e:
+        error_exit(fstr)
 
 
 def find(lst, key, value):
@@ -87,61 +86,40 @@ def find(lst, key, value):
 
 
 def form_repo_data(idx, remote_branch):
+    global contrib, msg, contrib_url, tag
     dbg_print("%s repo identified." % (repo_details[idx]['repo']))
     dbg_print("Your changes target the remote branch:%s" % (remote_branch))
-    global contrib, msg, contrib_url, tag, username, mvista_id
     contrib = repo_details[idx]['pushto']
     contrib_url = repo_details[idx]['url']
     msg = 'Merge to %s' % (remote_branch)
     tag = '%s-%s-V%s' % (remote_branch, bug_no, revision)
 
-    cmd = 'git config user.name > %s' % (tmp_f)
-    execute_cmd(cmd, "", "Please configure username for the git repo")
-    username = format_first_line(tmp_f)[0]
 
-    cmd = 'git config user.email > %s' % (tmp_f)
-    execute_cmd(cmd, "", "Please configure email-id for the git repo")
-    mvista_id = format_first_line(tmp_f)
+def identify_user():
+    global username, mvista_id
+    username = run_cmd('git config user.name', "Please configure username for the git repo").splitlines()[0]
+    mvista_id = run_cmd('git config user.email', "Please configure email-id for the git repo").splitlines()[0]
+    if not (re.match(r'.*.+@mvista.com', mvista_id)):
+        error_exit("User's email-ID is not from the domain mvista.com")
 
-    cmd = 'git config user.email | grep \'@mvista.com\' > %s' % (tmp_f)
-    execute_cmd(cmd, "", "User's email-ID is not from the domain mvista.com")
-
-    if (mvista_id):
-        mvista_id = mvista_id[0].split('@')[0]
-    else:
-        error_exit("Unable to fetch the required git repo details")
-
-
-def format_first_line(tfile):
-    txt = open(tfile)
-    tmp = txt.readline()
-    txt.close()
-    return tmp.splitlines()
+    mvista_id = mvista_id.split('@')[0]
 
 
 def identify_repo():
-    cmd = 'git config --get remote.origin.url > %s' % (tmp_f)
-    execute_cmd(cmd, "", "Please make sure that you are inside a valid git repository")
-    tmp = format_first_line(tmp_f)
-    if (tmp):
-        tmp = tmp[0].split('@')
-        if (len(tmp) != 2):
+    identify_user()
+    url = run_cmd('git config --get remote.origin.url', "Please make sure that you are inside a valid git repository").splitlines()[0]
+    match = re.compile(mvista_id+'@.*.')
+    if not (re.match(match, url)):
             error_exit("Not an MV type repo\n")
 
-        idx = find(repo_details, 'origin', tmp[1])
-        if (idx == -1):
-            error_exit("Unable to find the repo type\n")
+    idx = find(repo_details, 'origin', url.split('@')[1])
+    if (idx == -1):
+        error_exit("Unable to find the repo type\n")
 
-        # Now identify the remote tracking branch of the current branch
-        cmd = "git rev-parse --abbrev-ref --symbolic-full-name @{u} | cut -d'/' -f2- > %s" % (tmp_f)
-        execute_cmd(cmd, "", "")
-        tmp = format_first_line(tmp_f)
-        if (len(tmp) != 0):
-            form_repo_data(idx, tmp[0])
-        else:
-            error_exit("Unable to find the upstream branch for the current branch\n")
-    else:
-        error_exit("Not an MV type repo\n")
+    # Now identify the remote tracking branch of the current branch
+    remote_br = run_cmd('git rev-parse --abbrev-ref --symbolic-full-name @{u}', "Unable to find the remote branch").splitlines()[0]
+    remote_br = remote_br.split('/', 1)[1]
+    form_repo_data(idx, remote_br)
 
 
 def mark_start_commit():
@@ -156,7 +134,7 @@ def mark_start_commit():
 
 
 def parse_args():
-    global bug_no, revision, p_count, start, tmp_f, debug
+    global bug_no, revision, p_count, start, debug
     parser = argparse.ArgumentParser(description='Perform merge request')
     parser.add_argument('-b', help='Bug number', required=True, type=int)
     parser.add_argument('-r', help='Patch revision', required=True, type=int)
@@ -172,7 +150,6 @@ def parse_args():
     revision = str(args['r'])
     p_count = str(args['n'])
     start = str(args['s'])
-    tmp_f = bug_no+'-V'+revision+'.file'
 
 
 def check_err(err, got):
@@ -180,7 +157,7 @@ def check_err(err, got):
         error_exit(err)
 
 
-def update_bugz_fields(uname, pword):
+def update_bugz_fields(uname, pword, cmnt):
     bugz_login_url = 'http://bugz.mvista.com/show_bug.cgi?id='+bug_no
     bugz_post_url = 'http://bugz.mvista.com/process_bug.cgi'
 
@@ -205,10 +182,8 @@ def update_bugz_fields(uname, pword):
             soup = bs(r.text, 'lxml')
             upd_d['delta_ts'] = soup.find('input', {'name': 'delta_ts'}).get('value')
             upd_d['token'] = soup.find('input', {'name': 'token'}).get('value')
+            upd_d['comment'] = cmnt
 
-            txt = open(tmp_f)
-            upd_d['comment'] = txt.read()
-            txt.close()
             r = s.post(bugz_post_url, data=upd_d)
         except requests.exceptions.Timeout:
             error_exit("Connection timed out")
@@ -228,13 +203,10 @@ dbg_print("----------------------------------------")
 
 bugz_pword = getpass.getpass('Please enter bugzilla password for %s:' % (mvista_id))
 
-cmd = 'git tag -m "%s" "%s"' % (msg, tag)
-success_str = 'Successfully created tag : %s\n' % (tag)
-fail_str = 'Please try again after manually deleting the tag'
-execute_cmd(cmd, success_str, fail_str)
+run_cmd('git tag -m "%s" "%s"' % (msg, tag), 'Please try again after manually deleting the tag', 'tag: %s created\n' % (tag))
 tag_is_created = 1
 
-dbg_print("Pushing the tag : %s to %s" % (tag, contrib_url))
+dbg_print("Pushing the tag: %s to %s" % (tag, contrib_url))
 cmd = 'git push "%s"@"%s" "+%s"' % (mvista_id, contrib, tag)
 exp_str1 = 'password'
 exp_str2 = 'fatal: Could not read from remote repository.'
@@ -256,14 +228,9 @@ else:
     child.close()
     error_exit("Wrong password, Please try again..")
 
-if (debug == 1):
-    cmd = 'git request-pull %s %s %s | tee %s ; test ${PIPESTATUS[0]} -eq 0' % (start_c, contrib_url, tag, tmp_f)
-else:
-    cmd = 'git request-pull %s %s %s > %s' % (start_c, contrib_url, tag, tmp_f)
-
-success_str = 'Successfully generated pull request\n'
-execute_cmd(cmd, success_str, "")
+bugz_comment = run_cmd('git request-pull %s %s %s' % (start_c, contrib_url, tag))
+dbg_print(bugz_comment)
 
 dbg_print("Trying to update bugzilla fields for bug %s" % bug_no)
-update_bugz_fields(mvista_id, bugz_pword)
-print("Successfully posted the merge request on bugzilla, after pushing changes to contrib. Done.")
+update_bugz_fields(mvista_id, bugz_pword, bugz_comment)
+print("Successfully posted the merge request on bugzilla. Done.")
